a4db129305717bb78d0943fd5fbb5390370d6e84
Nick Holland <kungfunick.nh@googlemail.com>
2025-12-12 23:32:48 +0000

    Downgrade Tailwind to v3.4 for stability

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(async () => ({
-    plugins: [
-      react(),
-      tailwindcss(),
-    ],
-    server: {
-      port: 3000,
-      open: true
-    }
-  })
-)
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild'
+  }
+})
