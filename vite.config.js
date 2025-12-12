3cdf1848626afa5ee48ec3518df6dfd4b3243bbe
Nick Holland <kungfunick.nh@googlemail.com>
2025-12-12 17:50:33 +0000

    Fixed peer dependancy issue again?

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  }
-})
+export default defineConfig(async () => ({
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    server: {
+      port: 3000,
+      open: true
+    }
+  })
+)
